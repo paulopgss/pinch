4a393c29cc7e7d78408159c50dfe57594dbc53e3
Paulo Sousa <paulopgss@gmail.com>
2023-09-27 07:45:46 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Paulo Sousa on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
