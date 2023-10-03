1e55c3a2833a90a8863c972890c3ae38149070db
Paulo Sousa <paulopgss@gmail.com>
2023-10-02 23:05:12 -0300

    feat: :rocket: info panel and long press

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -26,6 +26,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -68,6 +69,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30),
+                alignment: .top
+                )
             
         }//: NAVIGATION
         .navigationViewStyle(.stack)
