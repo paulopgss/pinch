5367f77e138d754cee219b76fceccd5b17adee0e
Paulo Sousa <paulopgss@gmail.com>
2023-10-02 23:04:14 -0300

    feat: :rocket: info panel and long press

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Paulo Sousa on 02/10/23.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoPanelView()
+}
